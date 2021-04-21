a80038bcaf28234ed5f0f6b7b3fe01ac146e74e4
moeasakr <moeasakr@gmail.com>
2021-04-21 17:29:29 -0400

    first commit

---
+++ Database.php
@@ -0,0 +1,27 @@
+<?php
+namespace OnRoute\models;
+use PDO;
+class Database{
+    
+    private static $user ="moeasakr_the_webdevs";
+    private static $pass = "rk,O#Nuwn]wJ";
+    private static $dsn = "mysql:host=198.50.215.64;dbname=moeasakr_onroute";
+    
+    // private static $user ="root";
+    // private static $pass = "";//for mamp log in - Nai
+    // private static $dsn = "mysql:host=localhost;dbname=onroute";
+
+    private static $dbcon;
+
+    private function __construct() {
+
+    }
+
+    public static function getDB() {
+        if (!isset(self::$dbcon)) {
+            self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
+        }
+        return self::$dbcon;
+    }
+}
+
